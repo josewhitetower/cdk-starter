452ef682d6c9320da4b1140e69dacf2485b4b28d
Jose Torreblanca <jose.torreblanca@moia.io>
2024-01-30 19:25:14 +0100

    Add BucketTagger to CDK starter

--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -3,9 +3,14 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { PhotosStack } from './PhotosStack';
 import { PhotosHandlerStack } from './PhotosHandlerStack';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { BucketTagger } from './Tagger';
 
 const app = new cdk.App();
 const photosStack = new PhotosStack(app, 'PhotosStack');
 new PhotosHandlerStack(app, 'PhotosHandlerStack', {
     targetBucketArn: photosStack.photoBucketArn
 });
+
+const tagger = new BucketTagger('level', 'test');
+cdk.Aspects.of(app).add(tagger);
